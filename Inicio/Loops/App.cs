1c56bdd79193a23ee05b31221793b11b8b92be59
Gabriel Silva <gabriel-silva_2011@hotmail.com>
2017-10-02 14:50:00 -0300

    Loops

---
+++ Inicio/Loops/App.cs
@@ -0,0 +1,21 @@
+class App{
+    public static void Main(string[] args){
+    
+        // for(int i = 1; i <= 10; i++){
+        //     System.Console.WriteLine("i = " + i);
+        // }
+        
+        // int i = 1;
+        // while(i <= 10){
+        //     System.Console.WriteLine("i = " + i);
+        //     i++;
+        // }
+    
+        int i = 1;
+        do{
+            System.Console.WriteLine("i = " + i);
+            i++;
+        }while(i <= 10);
+    
+    }
+}
